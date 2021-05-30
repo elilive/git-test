8ab4ae74461abee5aa585e30bc36ff1847078ddb
Eli <1163730440@qq.com>
2021-05-30 14:58:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author EliChen
+ * @create 2021-05-30 14:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
